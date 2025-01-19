d7ff82767ae0a3928fabd9ffd634879ad27b3c15
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-01-19 18:01:31 +0600

    Create mysql.sh

---
+++ DataBase/MySQL/mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt install mysql-server -y
+#mysql_secure_installation
+#n/y
+#MySQLrootPA$$WORD
+#y
+#y
+#y
+#y
+#All done!
+#mysql
+#mysql>ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQLrootPA$$WORD';
+#mysql>CREATE USER 'sky'@'localhost' IDENTIFIED BY 'WhoDoYouTrust';
+#mysql>GRANT ALL PRIVILEGES ON *.* TO 'sky'@'localhost' WITH GRANT OPTION;
+#mysql>ALTER USER 'sky'@'localhost' IDENTIFIED WITH mysql_native_password BY 'WhoDoYouTrust';
+#mysql>FLUSH PRIVILEGES;
+#mysql>exit
+#Bye
