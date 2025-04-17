5ad8386837e539f43935f86cb596e63768afd02b
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-17 18:49:19 +0600

    Update AddUser.sh

--- ServerSecurity/AddUser.sh
+++ ServerSecurity/AddUser.sh
@@ -1,4 +1,4 @@
-#!usr/bin/bash
+#!/bin/bash
 # Check if the user variable is set
 if [ -z "$USER" ]; then
   echo "Error: The user variable is not set."
@@ -20,7 +20,23 @@ fi
 # Set user password
 { echo $USER_PASSWORD; echo $USER_PASSWORD; } | passwd $USER
 
+# Add user to sudo group
+usermod -aG sudo $USER
+
 # Allow user password authentication
 echo "Match User username" >> /etc/ssh/sshd_config
 echo "    PasswordAuthentication yes" >> /etc/ssh/sshd_config
 systemctl restart sshd
+
+# Configure SSH access
+mkdir /home/$USER/.ssh
+chmod 700 /home/$USER/.ssh
+cd /home/$USER/.ssh/
+
+# Put `id_rsa.pub` into user's `authorized_keys` file
+touch authorized_keys
+echo $USER_RSA_PUB >> /home/$USER/.ssh/authorized_keys
+
+# Set user rights
+chmod 400 /home/$USER/.ssh/authorized_keys
+chown $USER:$USER /home/$USER -R
